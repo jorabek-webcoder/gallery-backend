e396214fe88f2b9792f4724bc403527b69c2d387
jorabek-web <jorabekweb@gmail.com>
2026-02-05 21:13:55 +0500

    cors bug fix

--- src/index.js
+++ src/index.js
@@ -20,6 +20,7 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:5173",
+      "https://localhost:8685",
       "https://gallery-backend-g1ha.onrender.com",
     ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
